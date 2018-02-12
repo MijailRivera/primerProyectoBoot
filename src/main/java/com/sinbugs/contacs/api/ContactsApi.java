94f163a24ac249242ffef76020ef20940728fbb3
MijailRivera <mijail.rivera@gmail.com>
2018-02-11 23:40:40 -0500

    commit 2

--- src/main/java/com/sinbugs/contacs/api/ContactsApi.java
+++ src/main/java/com/sinbugs/contacs/api/ContactsApi.java
@@ -1,5 +1,6 @@
 package com.sinbugs.contacs.api;
 
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sinbugs.contacs.dto.Contact;
 import com.sinbugs.contacs.service.ContactService;
+import org.dozer.Mapper;
 
 @RestController
 public class ContactsApi {
@@ -15,14 +17,34 @@ public class ContactsApi {
 	@Autowired
 	ContactService contactService;
 	
+	// Inyecta mapper de Dozer
+	@Autowired
+	Mapper mapper;
+	
+//	@RequestMapping(value="/contact", method=RequestMethod.POST)
+//	public Contact updateOrSave(@RequestBody Contact contact){
+//		System.out.println("Hola Mundo!!");
+//	    return contactService.save(contact);
+//	}
+	
 	@RequestMapping(value="/contact", method=RequestMethod.POST)
-	public Contact updateOrSave(@RequestBody Contact contact){
-		System.out.println("Hola Mundo!!");
-	    return contactService.save(contact);
+	public ContactResponse updateOrSave(@RequestBody @Valid ContactRequest contactRequest){
+		// Mapeo request dto ==> entity
+		Contact contact = mapper.map(contactRequest, Contact.class);
+	    
+		// Invoca l[ogica de negocio
+		Contact updatedContact = contactService.save(contact);
+	    
+		// Mapeo entity ==> response dto
+	    ContactResponse contactResponse = mapper.map(updatedContact, ContactResponse.class); 
+	    
+	    return contactResponse;
 	}
 	
 	@RequestMapping(value="/contact", method=RequestMethod.GET)
 	public Contact getById(){
 		return new Contact(1L, "John", "Doe", "+57 311 222 3344", "john@sinbugs.com");
 	}
+	
+
 }
